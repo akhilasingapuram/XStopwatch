e159e9fdb2c2b49a7357e6ed275d2a503297c106
akhilasingapuram <singapuram.akhila@gmail.com>
2025-02-25 21:55:48 +0530

    updated

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -30,7 +30,7 @@ const StopWatch = () => {
   const formatTime = (time) => {
     const getSeconds = `0${time % 60}`.slice(-2);
     const minutes = `${Math.floor(time / 60)}`;
-    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getMinutes = `${minutes % 60}`.slice(-2);
     return `${getMinutes}:${getSeconds}`;
   };
 
